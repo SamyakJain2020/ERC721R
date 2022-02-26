cf0080061b372df19b09f0e338b53452ce6a7af1
Samyak Jain <samyakjainking@gmail.com>
2022-02-26 13:39:25 +0530

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    rotate: {
+      '15': '15deg',
+      '[-15]': '-15deg',
+    }
   },
   plugins: [],
 }
